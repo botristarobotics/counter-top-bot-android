c3c4284af0b36c5da0a9fcffd34280697c5371f9
Fantasy <fantasy.fang@botrista.io>
2024-12-10 14:20:14 +0800

    modify register response

--- app/src/main/java/com/botrista/countertopbot/remote/register/RegisterResponse.kt
+++ app/src/main/java/com/botrista/countertopbot/remote/register/RegisterResponse.kt
@@ -6,8 +6,14 @@ import com.google.gson.annotations.SerializedName
 data class RegisterResponse(
     @SerializedName("code")
     val code: Int,
-    @SerializedName("data")
-    val infoData: String,
     @SerializedName("status")
-    val status: Boolean
+    val status: Boolean,
+    @SerializedName("machine_id")
+    val machineId: String,
+    @SerializedName("serial_num")
+    val serialNum: String,
+    @SerializedName("public_key")
+    val publicKey: String,
+    @SerializedName("_id")
+    val id: String,
 )
